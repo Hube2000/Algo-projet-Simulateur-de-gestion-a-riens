78c735ca1911b275d5fe746401a9f159501de661
Az0x <theo.bonnier11@gmail.com>
2025-11-20 15:22:57 +0100

    V1 (AVEC UN .H à la place d'un .c c'est mieux ...

--- model/file.c
+++ model/file.c
@@ -1,15 +1,15 @@
-#include <stlio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "/model/avion.c"
-#include "/model/piste.c"
+#include "avion.h"
 
 avion * creerAvion(){
     avion * newAvion = malloc(sizeof(avion));
-    newAvion->identifiant = rand() % 51 + 1; 
-    newAvion->categorie = rand() % 3;
+    if(!newAvion) return NULL;
+    newAvion->identifiant = rand() % 51 + 1;
+    newAvion->categorie = (CATEGORIE_AVION)(rand() % 3);
     newAvion->etat = 0;
-    newAvion->nombre_de_passagers =0;
+    newAvion->nombre_de_passagers = 0;
     newAvion->next = NULL;
     newAvion->prev = NULL;
     return newAvion;
@@ -17,42 +17,51 @@ avion * creerAvion(){
 
 AvionFile *creerAvionFile(){
     AvionFile *file = malloc(sizeof(AvionFile));
+    if(!file) return NULL;
     file->nbElement = 0;
     file->premier = NULL;
     file->dernier = NULL;
     return file;
 }
 
-AvionFile ajouterDebutFile(AvionFile *file, avion *avion){
-    
+AvionFile * ajouterDebutFile(AvionFile *file, avion *a){
+    if(!file || !a) return file;
     if(file->nbElement == 0){
-        file->premier = avion;
-        file->dernier = avion;
+        file->premier = a;
+        file->dernier = a;
+        a->next = NULL;
+        a->prev = NULL;
     } else {
-        newAvion->next = file->premier;
-        file->premier->prev = avion;
-        file->premier = avion;
+        a->next = file->premier;
+        a->prev = NULL;
+        file->premier->prev = a;
+        file->premier = a;
     }
     file->nbElement++;
     return file;
 
 }
 
-AvionFile ajouterFinFile(AvionFile *file, avion *avion){
+AvionFile * ajouterFinFile(AvionFile *file, avion *a){
+    if(!file || !a) return file;
     if(file->nbElement == 0){
-        file->premier = avion;
-        file->dernier = avion;
+        file->premier = a;
+        file->dernier = a;
+        a->next = NULL;
+        a->prev = NULL;
     } else {
-        file->dernier->next = avion;
-        newAvion->prev = file->dernier;
-        file->dernier = avion;
+        file->dernier->next = a;
+        a->prev = file->dernier;
+        a->next = NULL;
+        file->dernier = a;
     }
     file->nbElement++;
     return file;
 }
 
 
-AvionFile supprimerDebutFile(AvionFile *file){
+
+AvionFile * supprimerDebutFile(AvionFile *file){
     if(file->nbElement == 0){
         return file;
     } else {
@@ -69,7 +78,7 @@ AvionFile supprimerDebutFile(AvionFile *file){
     return file;
 }
 
-AvionFile supprimerFinFile(AvionFile *file){
+AvionFile *supprimerFinFile(AvionFile *file){
     if(file->nbElement == 0){
         return file;
     } else {
@@ -84,4 +93,16 @@ AvionFile supprimerFinFile(AvionFile *file){
         file->nbElement--;
     }
     return file;
+}
+
+avion * rechercherAvion(AvionFile *file, int id){ 
+    if(!file) return NULL;
+    avion *current = file->premier;
+    while(current != NULL){
+        if(current->identifiant == id){
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
 }
