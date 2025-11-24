040b37cd26259b29ea791acaf299907a52c9daef
Az0x <theo.bonnier11@gmail.com>
2025-11-24 15:15:45 +0100

    update

--- models/aeroport.h
+++ models/aeroport.h
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include ".file.c"
-#include "avion.c"
-#include "piste.c"
+#include "avion.h"
+#include "piste.h"
 
 typedef struct Aeroport {
   PISTE *pistes[3];
-  Avion *parking[50];
+  avion *parking[50];
   int places;
   int passagers;
   int departs;
